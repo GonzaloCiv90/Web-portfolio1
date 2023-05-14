93557c3cf0453b434864f456e02ade8f4d3de702
Gonzalo Civita <gonzalocivita@gmail.com>
2023-05-14 00:08:03 -0300

    Se termina HTML y CSS, se agrega JS

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+let menuVisible = false; 
+//Función que oculta o muestra el menú
+function mostrarOcultarMenu(){
+	if(menuVisible){
+		document.getElementById("nav").classList = "";
+		menuVisible = false;
+	} else{
+		document.getElementById("nav").classList = "responsive";
+		menuVisible = true;
+	}
+}
+function seleccionar(){
+	//oculto el menú una vez que selecciono una opción
+	document.getElementById("nav").classList = "";
+	menuVisible = false; 
+}
+//Función que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+	var skills = document.getElementById("skills");
+	var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+	if(distancia_skills >= 300){
+		let habilidades = document.getElementsByClassName("progreso");
+		habilidades[0].classList.add("javascript");
+		habilidades[1].classList.add("htmlcss");
+		habilidades[2].classList.add("bootstraps");
+		habilidades[3].classList.add("phpmysql");
+		habilidades[4].classList.add("python");
+		habilidades[5].classList.add("equipo");
+		habilidades[6].classList.add("comunicacion");
+		habilidades[7].classList.add("creatividad");
+		habilidades[8].classList.add("dedicacion");
+		habilidades[9].classList.add("copywriting");
+	}
+}
 
+//Detecto el scrolling para las barras de las habilidades
+window.onscroll = function(){
+	efectoHabilidades();
+}
